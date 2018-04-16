09adcd81b7aaad427b7268e117c0236033c9ec64
lucasvm <lucas.ansalone@pinacourt.com>
2018-04-16 16:04:14 -0300

    Example how to add Custom Menu in Magento 2

---
+++ app/code/Digital/Importer/Controller/Adminhtml/Content/Info.php
@@ -0,0 +1,42 @@
+<?php
+namespace Stack\Example\Controller\Adminhtml\Content;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Info extends \Magento\Backend\App\Action
+{
+    const ADMIN_RESOURCE = 'Digital_Importer::content';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Update the breadcrumb and extra information
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->addBreadcrumb(__('Digital Importer'), __('Content'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Content'));
+
+        return $resultPage;
+    }
+}
